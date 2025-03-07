5c7616136a3c97270fc00099505f8beeb2ff252e
Francesco <bizzafrancesco@gmail.com>
2025-03-07 17:13:29 +1100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from dash import Dash, html
+
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+     html.H1(children='Hello dash!'),
+     html.Div(children='''
+         XGeoAI first commit
+     '''),
+ ])
+if __name__ == '__main__':
+    app.run_server(debug=True)
