fe544294907cf50779c1a3d8c9bfcbdb15267959
Francesco <bizzafrancesco@gmail.com>
2025-03-10 16:37:32 +1100

    Add transmission lines layer to Folium map

--- folium/folium_example.py
+++ folium/folium_example.py
@@ -1,5 +1,6 @@
 import folium
 import pandas as pd
+import json
 import webbrowser
 
 # TODO: check working directory and adjust accordingly root to data
@@ -7,24 +8,43 @@ import webbrowser
 # cwd = os.getcwd()
 # print(cwd)
 
+
 df = pd.read_csv('../data/processed/wind-farms.csv')
 
 m = folium.Map(location=[df['Latitude'].mean(), df['Longitude'].mean()], zoom_start=4)
 
+# add wind farm markers
 for idx, row in df.iterrows():
     popup = folium.Popup(row['Asset'], max_width=300)
 
     folium.CircleMarker(
         location=[row['Latitude'], row['Longitude']],
-        radius=5,
+        radius=4,
         stroke=False,
         fill=True,
         fill_opacity=1,
-        fill_color="green",
+        fill_color="orange",
         tooltip=row['Asset']
     ).add_to(m)
 
-map_path = 'map_with_minimal_markers.html'
-m.save(map_path)
+geojson_file = '../data/raw/Electricity_Transmission_Lines.geojson'
+with open(geojson_file, "r") as f:
+    geojson_data = json.load(f)
 
-webbrowser.open(f'{map_path}')
+# add the transmission lines to the map
+folium.GeoJson(
+    geojson_data,
+    name="Electricity Transmission Lines",
+    style_function=lambda x: {
+        # "color": "blue",
+        "weight": 2,
+        "opacity": 0.8
+    }
+).add_to(m)
+
+folium.LayerControl().add_to(m)
+
+map_path = 'folium_map.html'
+m.save(map_path)
+webbrowser.open(map_path)
+
--- scripts/process_transmission_lines.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-
-
-geojson_file = '../data/raw/Electricity_Transmission_Lines.geojson'
-gdf = gpd.read_file(geojson_file)
-
-
-print(gdf.head())
-
