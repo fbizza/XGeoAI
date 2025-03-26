ec5525f68f16b8451f7b9130c153a5b3013fca96
Francesco <bizzafrancesco@gmail.com>
2025-03-26 17:02:33 +1100

    Start working on ERA5 data

---
+++ src/era5/api-requests.py
@@ -0,0 +1,55 @@
+import json
+import cdsapi
+import webbrowser
+
+def get_era5_api_key():
+    try:
+        with open("../../api-keys", "r") as file:
+            api_keys = json.load(file)
+
+        era5_api_key = api_keys.get("ERA5_API_KEY")
+
+        if era5_api_key:
+            print(f"ERA5 API key: {era5_api_key}")
+            return era5_api_key
+        else:
+            raise KeyError("No ERA5 key was found in the keys text file")
+
+    except FileNotFoundError:
+        print("Error: keys text file not found")
+    except json.JSONDecodeError:
+        print("Error: keys text file is not a valid JSON dictionary")
+    except KeyError as e:
+        print(e)
+
+def era5_api_request():
+    dataset = "reanalysis-era5-single-levels"
+    request = {
+        "product_type": ["reanalysis"],
+        "year": ["2025"],
+        "month": ["01"],
+        "day": [
+            "01"
+        ],
+        "time": [
+            "00:00", "01:00", "02:00",
+            "03:00", "04:00", "05:00",
+            "06:00", "07:00", "08:00",
+            "09:00", "10:00", "11:00",
+            "12:00", "13:00", "14:00",
+            "15:00", "16:00", "17:00",
+            "18:00", "19:00", "20:00",
+            "21:00", "22:00", "23:00"
+        ],
+        "data_format": "netcdf",
+        "download_format": "unarchived",
+        "variable": [
+            "100m_u_component_of_wind",
+            "100m_v_component_of_wind"
+        ],
+        "area": [-10, 112, -44, 155]
+    }
+
+    client = cdsapi.Client(url="https://cds.climate.copernicus.eu/api", key=get_era5_api_key())
+    client.retrieve(dataset, request).download()
+
---
+++ src/era5/map.py
@@ -0,0 +1,100 @@
+import dash
+from dash import dcc, html
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import xarray as xr
+import random
+import geopandas as gpd
+from shapely.geometry import Point
+
+# Replace with the path to your downloaded .nc file and GeoJSON file for Australia
+file_path = 'wind-data.nc'
+geojson_file = '../../data/raw/countries-geojson.geojson'
+
+# Open the NetCDF file using xarray
+dataset = xr.open_dataset(file_path)
+
+# Access the wind components (u and v components of wind)
+u_wind = dataset['u100']
+v_wind = dataset['v100']
+
+# Check the dimension name related to time in your dataset
+time_dim = 'valid_time'  # This could be 'valid_time', or the name of the time dimension in your dataset
+
+# Access the data for the first time step
+u_wind_t0 = u_wind.isel({time_dim: 0}).values  #
+v_wind_t0 = v_wind.isel({time_dim: 0}).values
+
+# Get latitude and longitude values from the dataset
+lats = dataset['latitude'].values
+lons = dataset['longitude'].values
+
+# GeoJSON file for mainland Australia
+australia = gpd.read_file(geojson_file)
+
+australia_mainland = australia[australia['ADMIN'] == 'Australia']
+
+australia_mainland = australia_mainland.to_crs(epsg=4326)
+
+# to not crash the map
+num_points = 1000
+random_indices = random.sample(range(len(lats) * len(lons)), num_points)
+
+# get the corresponding latitude and longitude for the selected points
+selected_lats = [lats[i // len(lons)] for i in random_indices]
+selected_lons = [lons[i % len(lons)] for i in random_indices]
+
+# GeoDataFrame for the selected points
+selected_points = gpd.GeoDataFrame({
+    'geometry': [Point(lon, lat) for lon, lat in zip(selected_lons, selected_lats)]
+}, crs="EPSG:4326")
+
+# filter the points that lie within mainland Australia (MultiPolygon handling)
+selected_points = selected_points[selected_points.geometry.within(australia_mainland.union_all())]
+
+# get the wind data for the filtered points
+filtered_u_wind = [u_wind_t0[i // len(lons), i % len(lons)] for i in selected_points.index]
+filtered_v_wind = [v_wind_t0[i // len(lons), i % len(lons)] for i in selected_points.index]
+
+
+data = {
+    'lat': [point.y for point in selected_points.geometry],
+    'lon': [point.x for point in selected_points.geometry],
+    'u_wind': filtered_u_wind,
+    'v_wind': filtered_v_wind
+}
+
+df = pd.DataFrame(data)
+
+app = dash.Dash(__name__)
+
+
+fig = px.scatter_geo(df,
+                     lat='lat',
+                     lon='lon',
+                     color='u_wind',  # Color by the u_wind component
+                     color_continuous_scale='Viridis',  # Choose a color scale
+                     hover_name='u_wind',
+                     projection="mercator",
+                     title="Wind Data - u100 Component")
+
+
+fig.update_layout(
+    geo=dict(
+        showland=True,
+        landcolor="white",
+        showcoastlines=True,
+        coastlinecolor="Black",
+    ),
+)
+
+
+app.layout = html.Div([
+    html.H1("Wind Data Visualization"),
+    dcc.Graph(id='wind-map', figure=fig)
+])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
