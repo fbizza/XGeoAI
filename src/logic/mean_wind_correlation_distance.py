ee1aca12d0ef8a6919683617d6fd0daf1058fc3c
Francesco <bizzafrancesco@gmail.com>
2025-04-01 15:11:38 +1100

    Add scrips to compute distances in the correlation space

--- src/logic/mean_wind_correlation_distance.py
+++ src/logic/mean_wind_correlation_distance.py
@@ -1,12 +1,13 @@
 import numpy as np
-import pickle
 import pandas as pd
 import os
+from sklearn.metrics import pairwise_distances
+import pickle
 
-def process_and_save_df():
-    """Loads data, calculates mean correlations, and saves to a CSV with lon and lat"""
+def process_and_save_mean_distances():
+    """Loads data, calculates mean Euclidean distances in correlation space, and saves to a CSV with lon and lat."""
     data_folder = '../../data/raw/'
-    output_filepath = '../../data/basetables/mean_wind_correlation_df'
+    output_filepath = '../../data/basetables/mean_wind_correlation_distance_df'
 
     with open(os.path.join(data_folder, 'aus_region_mask.pkl'), 'rb') as f:
         lsmdf = pickle.load(f)
@@ -15,27 +16,26 @@ def process_and_save_df():
     with open(os.path.join(data_folder, 'wind_correlation_matrix.pkl'), 'rb') as f:
         rlsmcs5Wmnavg = pickle.load(f)
 
-    mean_correlations = np.mean(rlsmcs5Wmnavg, axis=1)
-    mean_correlation_map = np.empty(np.shape(lsmc)) * np.nan
+    pdist = pairwise_distances(rlsmcs5Wmnavg)
+    mean_distances = np.mean(pdist, axis=1)
     land_indices = np.argwhere(np.ndarray.flatten(lsmc) == 1)
     land_coords = np.unravel_index(land_indices.flatten(), np.shape(lsmc))
-    mean_correlation_map[land_coords] = mean_correlations
 
     longitude = lsmdf.longitude.values
     latitude = lsmdf.latitude.values
 
-    mean_correlations_flat = np.ndarray.flatten(mean_correlation_map)
-    mean_correlations_land = mean_correlations_flat[land_indices.flatten()]
+    mean_distances_map_flat = np.full(lsmc.size, np.nan)
+    mean_distances_map_flat[land_indices.flatten()] = mean_distances
+    mean_distances_land = mean_distances_map_flat[land_indices.flatten()]
 
     df = pd.DataFrame({
         'Latitude': latitude[land_coords[0]],
         'Longitude': longitude[land_coords[1]],
-        'Mean Correlation': mean_correlations_land
+        'Mean Distance': mean_distances_land
     })
 
     df.to_csv(output_filepath, index=False)
     print(f"DataFrame saved to: {output_filepath}")
 
 if __name__ == "__main__":
-    process_and_save_df()
-
+    process_and_save_mean_distances()
